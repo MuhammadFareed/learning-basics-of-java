61ac3e12b93830bf63b539dff66c8ee9e124e835
fareed <fareedalam873@gmail.com>
2022-10-09 23:19:39 +0500

    different types of variables

--- FirstClass.java
+++ FirstClass.java
@@ -1,13 +1,23 @@
 class FirstClass {
 
   public static void main(String args[]) {
-    System.out.println("Hello Woorld");
-    System.out.print("Hello Woorld");
-    System.out.println("Hey!");
-    int x;
-    x = 34;
-    int y = 37;
-    System.out.println(x);
-    System.out.println(y);
+    // System.out.println("Hello Woorld");
+    // System.out.print("Hello Woorld");
+    // System.out.println("Hey!");
+    // int x;
+    // x = 34;
+    // System.out.println(x);
+    // int y = 37;
+    // System.out.println(y);
+    // float f1 = 37.2234f;
+    // System.out.println(f1);
+    // double z = 372234;
+    // System.out.println(z);
+    // boolean z2 = true;
+    // System.out.println(z2);
+    // char z3 = 'a';
+    // System.out.println(z3);
+    // String z4 = "sdfsdf";
+    // System.out.println(z4);
   }
 }
