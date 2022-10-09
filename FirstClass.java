a91a7d6241e5f3d2129179f5f6b4ae4a9b36a9f0
fareed <fareedalam873@gmail.com>
2022-10-09 23:12:31 +0500

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,13 @@
+class FirstClass {
+
+  public static void main(String args[]) {
+    System.out.println("Hello Woorld");
+    System.out.print("Hello Woorld");
+    System.out.println("Hey!");
+    int x;
+    x = 34;
+    int y = 37;
+    System.out.println(x);
+    System.out.println(y);
+  }
+}
